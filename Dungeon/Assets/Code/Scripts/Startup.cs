7ace286fc3049994a8753a0d3863f2167d069bb8
joelyou20 <joelcribby@live.ca>
2022-04-17 22:54:31 -0700

    finish first draft of enemyAI

--- Dungeon/Assets/Code/Scripts/Startup.cs
+++ Dungeon/Assets/Code/Scripts/Startup.cs
@@ -8,17 +8,11 @@ using UnityEngine.SceneManagement;
 public class Startup : MonoBehaviour
 {
     public SceneNames FirstSceneName;
-    public GameObject Player;
+
+    private GameObject _player;
 
     void Start()
     {
-
         GameSceneManager.LoadScene(Enum.GetName(typeof(SceneNames), FirstSceneName));
-        SceneManager.sceneLoaded += SceneManager_sceneLoaded;
-    }
-
-    private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
-    {
-        Instantiate(Player, GameSceneManager.PlayerSpawn.transform);
     }
 }
